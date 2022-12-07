e5c7458a2c7c67259bbd8a9167881940a356f932
Manaswini2003 <20wh1a05b8@bvrithyderabad.edu.in>
2022-12-07 16:23:02 +0530

    Create 2.cpp

---
+++ Decision Making/2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ char x;
+ cout<<"enter a letter:";
+ cin>>x;
+ if(x =='a'|| x == 'e'|| x == 'i' || x=='o'|| x == 'u')
+ cout<< "Vowel"<<endl;
+ else
+ cout<<"Consonant"<<endl;
+ return 0;
+}
